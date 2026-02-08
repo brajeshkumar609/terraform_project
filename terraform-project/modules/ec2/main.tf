5a82713660138f6b55a3a2c703946f1c9e5f840f
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-08 13:20:37 +0530

    Refactor EC2 instance resource configuration
    Updated EC2 instance resource configuration to use ami_id directly and modified lifecycle rules.

--- terraform-project/modules/ec2/main.tf
+++ terraform-project/modules/ec2/main.tf
@@ -1,48 +1,30 @@
-#############################################
-# Fetch Latest Amazon Linux 2 AMI
-# This avoids hardcoding AMI IDs
-#############################################
+# Optional: if you prefer to always use the latest Amazon Linux 2 for the region,
+# you can use a data source and ignore the ami_id variable. For now, we use ami_id from root.
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_name
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  tags = merge(var.common_tags, {
+    Name = "${var.environment}-ec2"
+  })
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-#############################################
-# EC2 Instance Resource
-#############################################
-
-resource "aws_instance" "this" {
-
-  ami           = var.ami_id != "" ? var.ami_id : data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-
-  vpc_security_group_ids = var.security_group_ids
-
-  key_name = var.key_name
-
-  associate_public_ip_address = var.associate_public_ip
-
-  monitoring = true   # Enable detailed monitoring (production best practice)
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "${var.environment}-ec2"
-    }
-  )
-
-  #############################################
-  # Lifecycle Rules (Prevents Accidental Destroy)
-  #############################################
-  lifecycle {
-  prevent_destroy = true
+output "instance_id" {
+  value = aws_instance.this.id
 }
 
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.this.private_ip
 }
