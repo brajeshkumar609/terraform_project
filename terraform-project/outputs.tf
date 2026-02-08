1344ba44c739abff18e4dfd9160b1255cd284bf3
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-08 13:15:46 +0530

    Refactor outputs to use module values
    Replaced direct instance outputs with module outputs for better modularity and organization.

--- terraform-project/outputs.tf
+++ terraform-project/outputs.tf
@@ -1,11 +1,24 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "security_group_id" {
+  value = module.security_group.security_group_id
+}
+
 output "instance_id" {
-  value = aws_instance.this.id
+  value = module.ec2.instance_id
 }
 
-output "public_ip" {
-  value = aws_instance.this.public_ip
+output "instance_private_ip" {
+  value = module.ec2.private_ip
 }
 
-output "private_ip" {
-  value = aws_instance.this.private_ip
+output "instance_public_ip" {
+  value       = module.ec2.public_ip
+  description = "Will be null if instance is in private subnet with no public IP"
 }
