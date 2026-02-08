0e3769c121911e5a1c447281248753e4357facb5
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-08 11:42:02 +0530

    Add security group resource for SSH access
    Create a security group for allowing SSH access.

---
+++ terraform-project/modules/security-group/main.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "this" {
+  name        = "${var.environment}-sg"
+  description = "Allow SSH"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    { Name = "${var.environment}-sg" }
+  )
+}
