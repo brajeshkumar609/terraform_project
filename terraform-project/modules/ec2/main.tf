9c6d64e92cf2b24545532dbb6d1c8a7899f918f7
Brajesh Kumar <91620880+brajeshkumar609@users.noreply.github.com>
2026-02-17 11:57:33 +0530

    Add EBS optimization and monitoring to EC2 instance

--- terraform-project/modules/ec2/main.tf
+++ terraform-project/modules/ec2/main.tf
@@ -8,6 +8,10 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
+  ebs_optimized               = true     # CKV_AWS_135
+  monitoring                  = true     # CKV_AWS_1
+
+
   tags = merge(var.common_tags, {
     Name = "${var.environment}-ec2"
   })
@@ -28,3 +32,4 @@ output "public_ip" {
 output "private_ip" {
   value = aws_instance.this.private_ip
 }
+
