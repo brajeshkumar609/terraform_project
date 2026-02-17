231f341437af2ea0f9f5e3e3583c2dda3a740aa2
EC2 Default User <ec2-user@ip-172-31-93-204.ec2.internal>
2026-02-17 08:28:55 +0000

    almost done

--- terraform-project/modules/ec2/main.tf
+++ terraform-project/modules/ec2/main.tf
@@ -1,5 +1,4 @@
-# Optional: if you prefer to always use the latest Amazon Linux 2 for the region,
-# you can use a data source and ignore the ami_id variable. For now, we use ami_id from root.
+# modules/ec2/main.tf
 
 resource "aws_instance" "this" {
   ami                    = var.ami_id
@@ -8,9 +7,20 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
-  ebs_optimized = true # CKV_AWS_135
-  monitoring    = true # CKV_AWS_1
+  # Performance & observability
+  ebs_optimized = true                # CKV_AWS_135
+  monitoring    = true                # CKV_AWS_136 (detailed CloudWatch monitoring)
 
+  # Encrypt the root volume (CKV_AWS_8)
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 20
+    encrypted   = true
+    # kms_key_id = var.kms_key_id     # optional: set if using a customer-managed CMK
+  }
+
+  # Attach an IAM instance profile (CKV2_AWS_41)
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   tags = merge(var.common_tags, {
     Name = "${var.environment}-ec2"
@@ -21,15 +31,33 @@ resource "aws_instance" "this" {
   }
 }
 
-output "instance_id" {
-  value = aws_instance.this.id
+#######################################
+# Minimal IAM role + instance profile #
+#######################################
+
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.environment}-ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ec2.amazonaws.com" },
+      Action   = "sts:AssumeRole"
+    }]
+  })
 }
 
-output "public_ip" {
-  value = aws_instance.this.public_ip
+# Optional: SSM Session Manager access for the instance
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-output "private_ip" {
-  value = aws_instance.this.private_ip
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.environment}-ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
 }
 
+output "instance_id"  { value = aws_instance.this.id }
+output "public_ip"   { value = aws_instance.this.public_ip }
+output "private_ip"  { value = aws_instance.this.private_ip }
